3196afe4d9a28a0656e14bc2c190bd49f80dedcb
arguchinsky <arguchinsky55@gmail.com>
2019-10-07 09:37:34 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.mid = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
 
+        this.mid = Math.ceil((this.max + this.min)/ 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
